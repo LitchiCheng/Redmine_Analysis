722642039921b6e893c89a90891dbd519d4a27a5
darboy <974782852@qq.com>
2021-01-05 18:42:03 +0800

    modify

--- date_util.py
+++ date_util.py
@@ -46,3 +46,111 @@ def generateAllDayDict(years):
         a += 1
         all_date_dict[b] = 0
     return all_date_dict
+
+def generateAllDayList(years):
+    start_date = '%s-1-1' % years
+    a = 0
+    all_date_dict = []
+    days_sum = isLeapYear(int(years))
+    print()
+    while a < days_sum:
+        b = arrow.get(start_date).shift(days=a).format("YYYY-MM-DD")
+        a += 1
+        all_date_dict.append(b)
+    return all_date_dict
+
+def generateAllMonthList(years):
+    start_date = '%s-1-1' % years
+    a = 0
+    all_date_dict = []
+    days_sum = isLeapYear(int(years))
+    month_record = "01"
+    print()
+    while a < days_sum:
+        b = arrow.get(start_date).shift(days=a).format("YYYY-MM-DD")
+        a += 1
+        if str(b[5:7]) != month_record:
+            all_date_dict.append(b)
+        else:
+            all_date_dict.append("")
+        month_record = b[5:7]
+    return all_date_dict
+
+def generateAllHoursList():
+    start_hour = 00
+    all_hour_list = []
+    all_hour_list.append("00:00:00")
+    hours_sum = 24
+    step = 1
+    while step < hours_sum:
+        step += 1
+        start_hour += 1
+        hour_str = ""
+        if start_hour < 10:
+            hour_str += ('0'+str(start_hour) + ":00:00")
+        else:
+            hour_str += (str(start_hour) + ":00:00")
+        all_hour_list.append(hour_str)
+        hour_str = ""
+    return all_hour_list
+
+def generateAllHoursDict():
+    start_hour = 00
+    all_hour_dict = {}
+    all_hour_dict[start_hour] = 0
+    hours_sum = 24
+    step = 1
+    while step < hours_sum:
+        step += 1
+        start_hour += 1
+        all_hour_dict[start_hour] = 0
+    return all_hour_dict
+
+def transformTimeZone(o, n, time):
+    t_temp = time
+    time_zone_step = n - o
+    if time_zone_step > 0:
+        if (t_temp + time_zone_step) > 23:
+            t_temp += 8
+            t_temp -= 24
+        else:
+            t_temp += 8
+    return t_temp
+
+
+# print(generateAllHoursList())
+# print(generateAllHoursDict())
+# print(transformTimeZone(0,8,2))
+
+from redminelib import Redmine
+redmine = Redmine("https://dev.seer-group.com", username = "darboy", password = "3.14159265758ch")
+users = redmine.user.all()
+issues = redmine.issue.all()
+# print(dir(users[0])) 
+#['admin', 'created_on', 'firstname', 'groups', 'id', 'issues', 'last_login_on', 'lastname', 'login', 'mail', 'memberships', 'time_entries']
+# print(dir(issues[0])) 
+#['assigned_to', 'attachments', 'author', 'changesets', 'children', 'closed_on', 
+# 'created_on', 'custom_fields', 'description', 'done_ratio', 'due_date', 'estimated_hours', 'id', 
+# 'is_private', 'journals', 'priority', 'project', 'relations', 'start_date', 'status', 'subject', 'time_entries', 'tracker', 'updated_on', 'watchers']
+# print(dir(issues[0].author)) #['groups', 'id', 'issues', 'memberships', 'name', 'time_entries']
+# print((users[22].lastname + users[22].firstname)) 
+print(dir(issues[0].journals))
+# user_list = redmine.user.all()
+# print(len(user_list))
+# print((issues[0].author.name)) #['groups', 'id', 'issues', 'memberships', 'name', 'time_entries']
+# print((issues[0].project.name)) #['enabled_modules', 'files', 'id', 'issue_categories', 'issues', 'memberships', 'name', 'news', 'time_entries', 'time_entry_activities', 'trackers', 'versions', 'wiki_pages']
+
+# projects = redmine.project.all()
+# # print(dir(projects[0]))
+# # ['created_on', 'description', 'enabled_modules', 'files', 'id', 'identifier', 'is_public', 'issue_categories', 'issues', 'memberships', 'name', 'news', 'status', 'time_entries', 'time_entry_activities', 'trackers', 'updated_on', 'versions', 'wiki_pages']
+# sum = 0
+# for item in projects:
+#     try:
+#         print(len(item.issues))
+#         if item.id != 38 and item.id != 40:
+#             sum = sum + len(item.issues)
+#     except Exception as e:
+#         print(item.name)
+    
+#     # # print("name is %s, id is %s" % (item.name, item.id))
+# print(sum)
--- test.py
+++ test.py
@@ -26,28 +26,141 @@ class RedMineAnalysis:
     def getAllIssues(self):
         return self.redmine.issue.all()
 
+    def getAllUser(self):
+        return self.redmine.user.all()
+
     def sumIssuesNumByDate(self):
         self.date_dict = du.generateAllDayDict("2020")
         self.all_issues = self.getAllIssues()
+        self.date_list = []
+        self.num_list = []
         for item in self.all_issues:
             try:
-                key = str(item.created_on)
-                print(key)
-                if key in self.date_dict:
+                key = str(item.start_date)
+                self.date_list.append(key)
+                time = str(item.created_on)
+                project_id = int(item.project.id)
+                if key in self.date_dict and (str(time[0:4]) == "2020") and project_id != 40 and project_id != 38 and project_id != 46:
                     value = self.date_dict[key]
                     self.date_dict[key] = value + 1
-                else:
-                    self.date_dict[key] = 0
             except Exception as e:
                 print(e)
         last_value = 0
         for k in self.date_dict.keys():
-            self.date_dict[k] = self.date_dict[k] + last_value
+            # self.date_dict[k] = self.date_dict[k] + last_value    # 叠加效果
+            self.num_list.append(self.date_dict[k])
             last_value = self.date_dict[k]
+        print(du.generateAllMonthList('2020')) # 每月为节点的全年天数列表
+        print(self.num_list)
         return self.date_dict
+    
+    def sumIssuesCreater(self):
+        self.all_issues = self.getAllIssues()
+        self.username_list = []
+        self.num_list = []
+        self.username_dict = {}
+        self.username_num_list = []
+        for user in self.getAllUser():
+            user_name = str(user.lastname) + str(user.firstname)
+            self.username_dict[user_name] = 0
+            self.username_list.append(user_name)
+            self.username_num_list.append([user_name,0])
+            print(user_name)
+        for item in self.all_issues:
+            try:
+                key = str(item.author.name)  #
+                time = str(item.created_on)
+                project_id = int(item.project.id)
+                if key in self.username_dict and (str(time[0:4]) == "2020") and (project_id != 40) and (project_id != 38):
+                    value = self.username_dict[key]
+                    self.username_dict[key] = value + 1
+                    for ii in self.username_num_list:
+                        if ii[0] == key:
+                            ii[1] = value + 1
+            except Exception as e:
+                print(e)
+        for k in self.username_dict.keys():
+            self.num_list.append(self.username_dict[k])
+        print(self.username_list) # 所有用户
+        print(self.num_list)
+        print(self.username_num_list)
+        return self.username_dict
 
-test = RedMineAnalysis('https://xxm', "xxx", "3.xxxx")
-dict_result = test.sumIssuesNumByDate()
+    def sumIssuesNumByTime(self):
+        self.hours_dict = du.generateAllHoursDict()
+        self.all_issues = self.getAllIssues()
+        self.hours_list = du.generateAllHoursList()
+        self.num_list = []
+        for item in self.all_issues:
+            try:
+                key = str(item.created_on)  # 2019-12-18 05:42:23
+                item_hour = du.transformTimeZone(0,8,int(key[11:13]))
+                project_id = int(item.project.id)
+                if item_hour in self.hours_dict and (str(key[0:4]) == "2020") and project_id != 40 and project_id != 38 and project_id != 46:
+                    value = self.hours_dict[item_hour]
+                    self.hours_dict[item_hour] = value + 1
+            except Exception as e:
+                print(e)
+        # last_value = 0
+        for k in self.hours_dict.keys():
+            # self.hours_dict[k] = self.hours_dict[k] + last_value    # 叠加效果
+            self.num_list.append(self.hours_dict[k])
+            last_value = self.hours_dict[k]
+        print(self.hours_list) # 每月为节点的全年天数列表
+        print(self.num_list)
+        return self.hours_dict
+
+    def sumIssuesNumUpdatedByTime(self):
+        self.hours_dict = du.generateAllHoursDict()
+        self.all_issues = self.getAllIssues()
+        self.hours_list = du.generateAllHoursList()
+        self.num_list = []
+        for item in self.all_issues:
+            try:
+                key = str(item.updated_on)  # 2019-12-18 05:42:23
+                item_hour = du.transformTimeZone(0,8,int(key[11:13]))
+                project_id = int(item.project.id)
+                if item_hour in self.hours_dict and (str(key[0:4]) == "2020") and project_id != 40 and project_id != 38 and project_id != 46:
+                    value = self.hours_dict[item_hour]
+                    self.hours_dict[item_hour] = value + 1
+            except Exception as e:
+                print(e)
+        # last_value = 0
+        for k in self.hours_dict.keys():
+            # self.hours_dict[k] = self.hours_dict[k] + last_value    # 叠加效果
+            self.num_list.append(self.hours_dict[k])
+            last_value = self.hours_dict[k]
+        print(self.hours_list) # 每月为节点的全年天数列表
+        print(self.num_list)
+        return self.hours_dict
+    
+    def sumIssuesWatchers(self):
+        self.all_issues = self.getAllIssues()
+        self.num_list = []
+        self.id_list = []
+        self.id_watch_num_dict = {}
+        for item in self.all_issues:
+            try:
+                time = str(item.created_on)  # 2019-12-18 05:42:23
+                key = str(item.id)  # 2019-12-18 05:42:23
+                
+                project_id = int(item.project.id)
+                if(str(time[0:4]) == "2020") and project_id != 40 and project_id != 38 and project_id != 46:
+                    self.id_list.append(key)
+                    self.id_watch_num_dict[key] = len(item.watchers)
+            except Exception as e:
+                print(e)
+        # last_value = 0
+        for k in self.id_watch_num_dict.keys():
+            # self.hours_dict[k] = self.hours_dict[k] + last_value    # 叠加效果
+            self.num_list.append(self.id_watch_num_dict[k])
+            last_value = self.id_watch_num_dict[k]
+        print(self.id_list)
+        print(self.num_list)
+        return self.id_watch_num_dict
+
+test = RedMineAnalysis('xx', "xx", "3.xxx")
+dict_result = test.sumIssuesWatchers()
 print(dict_result)
 
 import pyqtgraph as pg
@@ -68,12 +181,4 @@ if __name__ == "__main__":
     import sys
     if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
         QtGui.QApplication.instance().exec_()
-
-# dd = {}
-# key = 9
-# if key in dd:
-#     print("fsadfas")
-# else:
-#     dd[key] = 10
-# print(dd.values())
 
